837b2f998cdd677d3026f4abff5dd2a0a441ab70
Laurenne <laurenneoliver@gmail.com>
2025-11-08 21:00:09 -0600

    styled microphone to let users know it is on

--- leaseease/src/components/ChatInput.js
+++ leaseease/src/components/ChatInput.js
@@ -4,7 +4,12 @@ function ChatInput({ isListening, onToggleListening, voiceSupported }){
   return (
     <div style={{position:'relative',height:120}}>
       <div style={{textAlign:'center',marginTop:8}}>
-        <button className="center-mic" onClick={onToggleListening} disabled={!voiceSupported} aria-label="Toggle speak">
+        <button
+          className={`center-mic${isListening ? ' is-listening' : ''}`}
+          onClick={onToggleListening}
+          disabled={!voiceSupported}
+          aria-label="Toggle speak"
+        >
           <svg width="44" height="44" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M12 14a3 3 0 0 0 3-3V6a3 3 0 0 0-6 0v5a3 3 0 0 0 3 3z" fill="#fff" />
             <path d="M19 11a1 1 0 0 0-2 0 5 5 0 0 1-10 0 1 1 0 0 0-2 0 7 7 0 0 0 6 6.92V21a1 1 0 1 0 2 0v-3.08A7 7 0 0 0 19 11z" fill="#fff" opacity="0.9" />
